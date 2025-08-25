fc32d451e50c506e47cf7b755ee6a20fd5cb8a7e
Joon080o <esjoon05@gmail.com>
2025-08-25 16:56:17 +0900

    배포용 빌드 추가

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 763, hash: '52b25fb9be37ec0dfae8d872264d609745a3916c1150c8398e7d9fa38a50ad24', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1276, hash: 'dcaf577bc3e8a21113f1ebc1d4d0e5e3aa20a9b74e06514e434d38d9f01a452f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 7260, hash: '4db0ef02f5f71a7d4551c4c60284f64960b7f6f89e6d7aedd87ee2092d67638b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 697, hash: 'a27217b1b73cd3e1e572342cf7d3fc09f7b14f1ff717b90029b874ba14f2aae4', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1210, hash: '7ed4a86737f6938c1dc1fec3b3562b2e22b86bc5b39d4eb4a1e6d517c9cbc3da', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 7621, hash: '7d40536b9bfc853ec9611a9d6a1db144ebd486add2b8cf462e44e852f7dfd0db', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
--- docs/server/assets-chunks/index_csr_html.mjs
+++ docs/server/assets-chunks/index_csr_html.mjs
@@ -2,14 +2,14 @@ export default `<!DOCTYPE html>
 <html lang="ko" data-beasties-container="">
 <head><base href="/seguk-wiki/">
   <meta charset="UTF-8">
-  <title>나무위키 표방 - 우리학교 이벤트 위키</title>
+  <title>세국 위키 - 2학년 4반</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="styles-5INURTSO.css"></head>
 <body ngcm="">
   <header style="background:#4caf50;padding:20px;color:#fff;text-align:center;">
-    <h1 style="margin:0;">우리학교 위키 (나무위키 표방)</h1>
-    <p style="margin:0;">이 사이트는 나무위키 스타일을 참고한 이벤트용 위키입니다.</p>
+    <h1 style="margin:0;">세국 위키 - 2학년 4반</h1>
+    <p style="margin:0;">혜경쌤의 계획으로 만들어진 세국 위키</p>
   </header>
   <app-root></app-root>
-<script src="polyfills-5CFQRCPP.js" type="module"></script><script src="main-LNUWIJIK.js" type="module"></script></body>
+<script src="polyfills-5CFQRCPP.js" type="module"></script><script src="main-MW2WE6DO.js" type="module"></script></body>
 </html>`;
