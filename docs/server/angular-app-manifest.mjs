b9a51761e37d4391526ed5e4638d4d9d4850fa2b
Joon080o <esjoon05@gmail.com>
2025-09-05 10:23:00 +0900

    배포용 빌드 추가

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 889, hash: 'e621b909df61d88f427c5e3afbe02723f40682925e98036a33f3b8b96baf0fb7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1210, hash: '8e51a1d3067bace3ba580b9eb1828e87fa718343ccea6edd4d0f1e1ed7b60329', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 8248, hash: '5d94fe31ca4835852553bf7adaf3acad4ea0e56403d2bdc388913b380348bc66', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 889, hash: 'a4c2d680d811fb0235c9b9311a5a5e0d0f106f25b9e4bfc7db2b79b27ae2554e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1210, hash: '5c506866450c3a66d8062686df191c9a1b21f5fab0dd58ec7a038f8e106b59a0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 8248, hash: '3969990a4f8993f3627d055a8b0c69050ee7c0f88f3e1e054789e2587b4ceb74', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-HQCIPD4C.css': {size: 67, hash: 'nibY493osCc', text: () => import('./assets-chunks/styles-HQCIPD4C_css.mjs').then(m => m.default)}
   },
 };
